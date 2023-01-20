04df9318ecc1bc3720ed980b4e08cc525e54c981
pakzad <pakzadlive@gmail.com>
2023-01-20 16:06:39 +0330

    working on the main navbar and hero section

--- components/home-page/hero.js
+++ components/home-page/hero.js
@@ -1,11 +1,19 @@
+import Image from "next/image";
+import classes from "./hero.module.css";
+
 function Hero() {
   return (
-    <section>
-      <div>Image</div>
-      <h1>Hi, This is a Text</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      </p>
+    <section className={classes.hero}>
+      <h2>Get rid of all of the stress that you might have</h2>
+      <button>Get the app</button>
+      <div className={classes.image}>
+        <Image
+          src="/images/phone-one.png"
+          alt="mobile image"
+          width={170}
+          height={342.4}
+        />
+      </div>
     </section>
   );
 }
