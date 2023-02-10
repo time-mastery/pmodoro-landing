b7a4338cf778b9d2aeab03d40cc9231a980e1366
pakzad <pakzadlive@gmail.com>
2023-02-10 20:28:43 +0330

    about section created

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
+import About from "@/components/home-page/about";
 import Hero from "@/components/home-page/hero";
 
 function HomePage() {
   return (
     <>
       <Hero />
+      <About />
     </>
   );
 }
