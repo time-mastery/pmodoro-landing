8141b6229bd6d9b4cabce2483536f95f91645c54
pakzad <pakzadlive@gmail.com>
2023-06-25 15:30:16 +0330

    Adding a condition to not show this field if the request is not successful

--- components/home-page/contributors.js
+++ components/home-page/contributors.js
@@ -13,43 +13,49 @@ export default function Contributors() {
   const { data } = useSwr("/api/github", fetcher);
 
   return (
-    <section className="text-center bg-gradient-to-b from-[#f1f1f1] to-[#f5f5f5] h-[210px] p-[10px]">
-      <h3 className="font-medium my-4 text-lg">
-        Contributors of <div className="inline font-bold">Pmodoro</div>
-      </h3>
-      {data?.contributors.map((contributor) => (
-        <section
-          key={contributor.id}
-          className="mb-[30px] flex items-center justify-center"
-        >
-          <Link href={contributor.html_url} target="_blank rel=noopener">
-            {contributor ? (
-              <div>
-                <img
-                  className="w-[50px] rounded-full ml-[5px] hover:w-[60px] hover:mb-[-10px]"
-                  src={contributor.avatar_url}
-                  alt={contributor.login}
-                />
-              </div>
-            ) : (
-              <Image src="/spinner.gif" alt="spinner" width={40} height={40} />
-            )}
+    <>
+      {data ? (
+        <section className="text-center bg-gradient-to-b from-[#f1f1f1] to-[#f5f5f5] h-[210px] p-[10px]">
+          <h3 className="font-medium my-4 text-lg">
+            Contributors of <div className="inline font-bold">Pmodoro</div>
+          </h3>
+          {data?.contributors.map((contributor) => (
+            <section
+              key={contributor.id}
+              className="mb-[30px] flex items-center justify-center"
+            >
+              <Link href={contributor.html_url} target="_blank rel=noopener">
+                {contributor ? (
+                  <div>
+                    <img
+                      className="w-[50px] rounded-full ml-[5px] hover:w-[60px] hover:mb-[-10px]"
+                      src={contributor.avatar_url}
+                      alt={contributor.login}
+                    />
+                  </div>
+                ) : (
+                  <Image src="/spinner.gif" alt="spinner" width={40} height={40} />
+                )}
 
-            {/* <Image
+                {/* <Image
               src={contributor.avatar_url}
               alt={contributor.login}
               width={30}
               height={30}
             /> */}
+              </Link>
+            </section>
+          ))}
+          <Link
+            href="https://github.com/time-mastery/pmodoro-application"
+            className="border-2 border-gray-800 rounded-3xl px-[26px] py-[7px] font-semibold transition-all duration-300 hover:bg-[#01ED64]"
+          >
+            Contribute
           </Link>
         </section>
-      ))}
-      <Link
-        href="https://github.com/time-mastery/pmodoro-application"
-        className="border-2 border-gray-800 rounded-3xl px-[26px] py-[7px] font-semibold transition-all duration-300 hover:bg-[#01ED64]"
-      >
-        Contribute
-      </Link>
-    </section>
+      ) : (
+        <div></div>
+      )}
+    </>
   );
 }
